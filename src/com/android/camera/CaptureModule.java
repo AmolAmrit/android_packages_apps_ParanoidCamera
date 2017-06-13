25ec7955b690adc960a87e51ce3d944bf85c396a
AmolAmrit <amol.amrit03@outlook.com>
2017-06-13 17:57:27 +0000

    Revert "SnapdragonCamera: Fix torch mode with continuous shot"
    This reverts commit f68cddc15c628574322d56d02653983dad2bc62e.

--- src/com/android/camera/CaptureModule.java
+++ src/com/android/camera/CaptureModule.java
@@ -682,7 +682,6 @@ public class CaptureModule implements CameraModule, PhotoController,
                 Integer afState = result.get(CaptureResult.CONTROL_AF_STATE);
                 Integer aeState = result.get(CaptureResult.CONTROL_AE_STATE);
                 Log.d(TAG, "STATE_WAITING_AF_LOCK id: " + id + " afState:" + afState + " aeState:" + aeState);
-
                 // AF_PASSIVE is added for continous auto focus mode
                 if (CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED == afState ||
                         CaptureResult.CONTROL_AF_STATE_NOT_FOCUSED_LOCKED == afState ||
@@ -698,20 +697,15 @@ public class CaptureModule implements CameraModule, PhotoController,
                         else
                             mState[id] = STATE_WAITING_AE_LOCK;
                     } else {
-                        if ((mLockRequestHashCode[id] == result.getRequest().hashCode()) || (mLockRequestHashCode[id] == 0)) {
-
-                            // CONTROL_AE_STATE can be null on some devices
-                            if(aeState == null || (aeState == CaptureResult
-                                    .CONTROL_AE_STATE_CONVERGED) && isFlashOff(id)) {
-                                lockExposure(id);
-                            } else {
-                                runPrecaptureSequence(id);
-                            }
+                        runPrecaptureSequence(id);
+                        // CONTROL_AE_STATE can be null on some devices
+                        if(aeState == null || (aeState == CaptureResult
+                                .CONTROL_AE_STATE_CONVERGED) && isFlashOff(id)) {
+                            lockExposure(id);
+                        } else {
+                            runPrecaptureSequence(id);
                         }
                     }
-                } else if (mLockRequestHashCode[id] == result.getRequest().hashCode()){
-                    Log.i(TAG, "AF lock request result received, but not focused");
-                    mLockRequestHashCode[id] = 0;
                 }
                 break;
             }
@@ -724,16 +718,8 @@ public class CaptureModule implements CameraModule, PhotoController,
                         aeState == CaptureResult.CONTROL_AE_STATE_PRECAPTURE ||
                         aeState == CaptureResult.CONTROL_AE_STATE_FLASH_REQUIRED ||
                         aeState == CaptureResult.CONTROL_AE_STATE_CONVERGED) {
-                    if ((mPrecaptureRequestHashCode[id] == result.getRequest().hashCode()) || (mPrecaptureRequestHashCode[id] == 0)) {
-                        if (mLongshotActive && isFlashOn(id)) {
-                            checkAfAeStatesAndCapture(id);
-                        } else {
-                            lockExposure(id);
-                        }
-                    }
-                } else if (mPrecaptureRequestHashCode[id] == result.getRequest().hashCode()) {
-                    Log.i(TAG, "AE trigger request result received, but not converged");
-                    mPrecaptureRequestHashCode[id] = 0;
+                    if (mPrecaptureRequestHashCode[id] == result.getRequest().hashCode())
+                        lockExposure(id);
                 }
                 break;
             }
@@ -1364,14 +1350,6 @@ public class CaptureModule implements CameraModule, PhotoController,
             if(id == MONO_ID && !canStartMonoPreview()) {
                 mCaptureSession[id].setRepeatingRequest(mPreviewRequestBuilder[id]
                         .build(), mCaptureCallback, mCameraHandler);
-            } else {
-                // for longshot flash, need to re-configure the preview flash mode.
-                if (mLongshotActive && isFlashOn(id)) {
-                    mCaptureSession[id].stopRepeating();
-                    applyFlash(mPreviewRequestBuilder[id], id);
-                    mCaptureSession[id].setRepeatingRequest(mPreviewRequestBuilder[id]
-                                            .build(), mCaptureCallback, mCameraHandler);
-                }
             }
         } catch (CameraAccessException e) {
             e.printStackTrace();
@@ -1381,7 +1359,6 @@ public class CaptureModule implements CameraModule, PhotoController,
         if (mState[id] == STATE_WAITING_TOUCH_FOCUS) {
             mCameraHandler.removeMessages(CANCEL_TOUCH_FOCUS, mCameraId[id]);
             mState[id] = STATE_WAITING_AF_LOCK;
-            mLockRequestHashCode[id] = 0;
             return;
         }
 
@@ -1685,7 +1662,6 @@ public class CaptureModule implements CameraModule, PhotoController,
             applySettingsForPrecapture(builder, id);
             CaptureRequest request = builder.build();
             mPrecaptureRequestHashCode[id] = request.hashCode();
-
             mState[id] = STATE_WAITING_PRECAPTURE;
             mCaptureSession[id].capture(request, mCaptureCallback, mCameraHandler);
         } catch (CameraAccessException | IllegalStateException e) {
@@ -3901,11 +3877,6 @@ public class CaptureModule implements CameraModule, PhotoController,
     private boolean isFlashOff(int id) {
         if (!mSettingsManager.isFlashSupported(id)) return true;
         return mSettingsManager.getValue(SettingsManager.KEY_FLASH_MODE).equals("off");
-    }
-
-    private boolean isFlashOn(int id) {
-        if (!mSettingsManager.isFlashSupported(id)) return false;
-        return mSettingsManager.getValue(SettingsManager.KEY_FLASH_MODE).equals("on");
     }
 
     private void initializePreviewConfiguration(int id) {
